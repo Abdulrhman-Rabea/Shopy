615edcffa81b8dd3096d9dbab560262150abb6f3
Yasmin Muhammad <yasminmuhammad219@gmail.com>
2025-08-15 05:05:34 +0300

    Created navbar and fetched it to home

---
+++ scripts/navbar.js
@@ -0,0 +1,15 @@
+// تحميل الnavbar
+fetch('Navbar.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('navbar').innerHTML = data;
+    })
+    .catch(err => console.error('فشل تحميل Navbar:', err));
+
+//navbar functions 
+function openSideBar() {
+    document.querySelector('.sideBar').style.display = "flex";
+}
+function closeSideBar() {
+    document.querySelector('.sideBar').style.display = "none";
+}
